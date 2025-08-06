8ea98c6d1305e2918be70e4aae4fb99e6eb5fb2d
devinward5 <devinward02@gmail.com>
2025-08-06 13:05:26 -0500

    Delete mlb-game-tracker directory

--- mlb-game-tracker/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <main className="flex min-h-screen items-center justify-center bg-white text-black">
-      <h1 className="text-4xl font-bold">MLB Game Tracker</h1>
-    </main>
-  );
-}
